5e8ca1b0e27cf0f8a78b56e1b66b1c79b465f8e8
Rodolfo Miguel Esperança Dos Santos Barreira <2180714@my.ipleiria.pt>
2019-05-06 16:49:04 +0100

    Adicionar Versões da aplicaçao

---
+++ Aplicações/Versao 23-04-2019/Bookids/Bookids/ClientesForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Bookids
+{
+    partial class ClientesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClientesForm));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAcederCliente = new System.Windows.Forms.Button();
+            this.btnAlterarCliente = new System.Windows.Forms.Button();
+            this.btnApagarCliente = new System.Windows.Forms.Button();
+            this.btnCriarClientes = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.BackColor = System.Drawing.Color.Bisque;
+            this.groupBox1.Controls.Add(this.btnAcederCliente);
+            this.groupBox1.Controls.Add(this.btnAlterarCliente);
+            this.groupBox1.Controls.Add(this.btnApagarCliente);
+            this.groupBox1.Controls.Add(this.btnCriarClientes);
+            this.groupBox1.Location = new System.Drawing.Point(80, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(631, 391);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gestão de Clientes";
+            // 
+            // btnAcederCliente
+            // 
+            this.btnAcederCliente.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAcederCliente.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnAcederCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAcederCliente.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAcederCliente.Location = new System.Drawing.Point(332, 228);
+            this.btnAcederCliente.Name = "btnAcederCliente";
+            this.btnAcederCliente.Size = new System.Drawing.Size(213, 79);
+            this.btnAcederCliente.TabIndex = 11;
+            this.btnAcederCliente.Text = "Dados do Cliente";
+            this.btnAcederCliente.UseVisualStyleBackColor = false;
+            // 
+            // btnAlterarCliente
+            // 
+            this.btnAlterarCliente.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAlterarCliente.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnAlterarCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlterarCliente.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAlterarCliente.Location = new System.Drawing.Point(85, 228);
+            this.btnAlterarCliente.Name = "btnAlterarCliente";
+            this.btnAlterarCliente.Size = new System.Drawing.Size(213, 79);
+            this.btnAlterarCliente.TabIndex = 10;
+            this.btnAlterarCliente.Text = "Alterar Cliente";
+            this.btnAlterarCliente.UseVisualStyleBackColor = false;
+            // 
+            // btnApagarCliente
+            // 
+            this.btnApagarCliente.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnApagarCliente.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnApagarCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApagarCliente.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnApagarCliente.Location = new System.Drawing.Point(332, 84);
+            this.btnApagarCliente.Name = "btnApagarCliente";
+            this.btnApagarCliente.Size = new System.Drawing.Size(213, 79);
+            this.btnApagarCliente.TabIndex = 9;
+            this.btnApagarCliente.Text = "Apagar Cliente";
+            this.btnApagarCliente.UseVisualStyleBackColor = false;
+            // 
+            // btnCriarClientes
+            // 
+            this.btnCriarClientes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCriarClientes.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnCriarClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCriarClientes.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCriarClientes.Location = new System.Drawing.Point(85, 84);
+            this.btnCriarClientes.Name = "btnCriarClientes";
+            this.btnCriarClientes.Size = new System.Drawing.Size(213, 79);
+            this.btnCriarClientes.TabIndex = 8;
+            this.btnCriarClientes.Text = "Novo Cliente";
+            this.btnCriarClientes.UseVisualStyleBackColor = false;
+            // 
+            // ClientesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ClientesForm";
+            this.Text = "Clientes";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAcederCliente;
+        private System.Windows.Forms.Button btnAlterarCliente;
+        private System.Windows.Forms.Button btnApagarCliente;
+        private System.Windows.Forms.Button btnCriarClientes;
+    }
+}
---
+++ Aplicações/Versao 23-04-2019/Bookids/Bookids/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Bookids
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnEventos = new System.Windows.Forms.Button();
+            this.btnAnimadores = new System.Windows.Forms.Button();
+            this.btnEscola = new System.Windows.Forms.Button();
+            this.btnClientes = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.BurlyWood;
+            this.groupBox1.Controls.Add(this.btnEventos);
+            this.groupBox1.Controls.Add(this.btnAnimadores);
+            this.groupBox1.Controls.Add(this.btnEscola);
+            this.groupBox1.Controls.Add(this.btnClientes);
+            this.groupBox1.Location = new System.Drawing.Point(156, 65);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(504, 380);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Area de Administrador";
+            // 
+            // btnEventos
+            // 
+            this.btnEventos.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnEventos.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEventos.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEventos.Location = new System.Drawing.Point(281, 220);
+            this.btnEventos.Name = "btnEventos";
+            this.btnEventos.Size = new System.Drawing.Size(201, 79);
+            this.btnEventos.TabIndex = 7;
+            this.btnEventos.Text = "Eventos";
+            this.btnEventos.UseVisualStyleBackColor = false;
+            // 
+            // btnAnimadores
+            // 
+            this.btnAnimadores.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnAnimadores.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnimadores.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAnimadores.Location = new System.Drawing.Point(34, 220);
+            this.btnAnimadores.Name = "btnAnimadores";
+            this.btnAnimadores.Size = new System.Drawing.Size(201, 79);
+            this.btnAnimadores.TabIndex = 6;
+            this.btnAnimadores.Text = "Animadores";
+            this.btnAnimadores.UseVisualStyleBackColor = false;
+            this.btnAnimadores.Click += new System.EventHandler(this.btnAnimadores_Click);
+            // 
+            // btnEscola
+            // 
+            this.btnEscola.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnEscola.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEscola.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEscola.Location = new System.Drawing.Point(281, 76);
+            this.btnEscola.Name = "btnEscola";
+            this.btnEscola.Size = new System.Drawing.Size(201, 79);
+            this.btnEscola.TabIndex = 5;
+            this.btnEscola.Text = "Escolas";
+            this.btnEscola.UseVisualStyleBackColor = false;
+            this.btnEscola.Click += new System.EventHandler(this.btnEscola_Click);
+            // 
+            // btnClientes
+            // 
+            this.btnClientes.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClientes.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnClientes.Location = new System.Drawing.Point(34, 76);
+            this.btnClientes.Name = "btnClientes";
+            this.btnClientes.Size = new System.Drawing.Size(201, 79);
+            this.btnClientes.TabIndex = 4;
+            this.btnClientes.Text = "Clientes";
+            this.btnClientes.UseVisualStyleBackColor = false;
+            this.btnClientes.Click += new System.EventHandler(this.btnClientes_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(843, 534);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Bookids";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnEventos;
+        private System.Windows.Forms.Button btnAnimadores;
+        private System.Windows.Forms.Button btnEscola;
+        private System.Windows.Forms.Button btnClientes;
+    }
+}
+
