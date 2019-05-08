e50cccc4270ec25acd85c9de071063305f0630d9
Miguel <miguel.m@hotmail.com>
2019-05-08 11:32:54 +0100

    Nova versao com correçao de erros

--- Aplicações/Versao 29-04-2019 com base de dados/Bookids/BookidsDB.Designer.cs
+++ Aplicações/Versao 29-04-2019 com base de dados/Bookids/BookidsDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\2180714\Desktop\Bookids\Bookids\BookidsDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\MiguelAlexandreInáci\bookids\Aplicações\Versao 29-04-2019 com base de dados\Bookids\BookidsDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
